a251c27433c26da29d161a3267c0f5782a64e42b
Kedaar-NR <kedaarnr@berkeley.edu>
2025-10-01 10:00:00 +0000

    feat: add Desktop component

---
+++ src/components/Desktop.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import { useWindowStore } from "@/store/windowStore";
+import MenuBar from "./MenuBar";
+import DesktopIcons from "./DesktopIcons";
+import Dock from "./Dock";
+import FinderWindow from "./FinderWindow";
+import MailWindow from "./MailWindow";
+import PortfolioWindow from "./PortfolioWindow";
+
+export default function Desktop() {
+  const { windows } = useWindowStore();
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  // Update time every second
+  useState(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div className="relative w-screen h-screen overflow-hidden">
+      {/* Background */}
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{
+          backgroundImage: `url('/images/wave-background.jpg')`,
+          backgroundAttachment: "fixed",
+        }}
+      />
+
+      {/* Menu Bar */}
+      <MenuBar currentTime={currentTime} />
+
+      {/* Desktop Icons */}
+      <DesktopIcons />
+
+      {/* Dock */}
+      <Dock />
+
+      {/* Windows */}
+      {windows.map((window) => {
+        if (!window.isOpen || window.isMinimized) return null;
+
+        const commonProps = {
+          key: window.id,
+          window,
+        };
+
+        switch (window.type) {
+          case "finder":
+            return <FinderWindow {...commonProps} />;
+          case "mail":
+            return <MailWindow {...commonProps} />;
+          case "portfolio":
+            return <PortfolioWindow {...commonProps} />;
+          default:
+            return null;
+        }
+      })}
+    </div>
+  );
+}
