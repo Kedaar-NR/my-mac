0f8c7f50a908249be18ba61d56e32bfb6d15b903
Kedaar-NR <kedaarnr@berkeley.edu>
2025-10-02 05:07:25 -0700

    config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // @ts-expect-error turbopack is not yet in the NextConfig type
   turbopack: {
     root: __dirname,
   },
